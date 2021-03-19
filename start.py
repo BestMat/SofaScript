cda43fe188432a929d019c0657535fc628b33781
BestMat <76582849+BestMat@users.noreply.github.com>
2021-03-19 10:04:36 +0530

    Creating start.py

---
+++ start.py
@@ -0,0 +1,7 @@
+print("SofaScript.js - Simplified JavaScript with Terminal")
+print("Installation Complete! - Get Started")
+a = input("Want to create a new project? (y/n)")
+if a == "y":
+    fname = input("Enter the file name: ")
+    f = open(fname, "x")
+    print("Created file successfully!")
